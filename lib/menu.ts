01a0e2c3750880fd5882b10c197161432e40774f
Shawn Sob <shawnsob8@gmail.com>
2026-07-20 14:40:52 +0200

    Ajout des boissons au menu

--- lib/menu.ts
+++ lib/menu.ts
@@ -1,29 +1,30 @@
 export const menu = [
+  // PLATS
   {
     id: 1,
     name: "Poulet Yassa",
-    description: "Poulet mariné avec oignons et sauce citronnée",
+    description: "Poulet mariné, oignons, citron, moutarde, huile d’olive, ail et thym",
     price: 12.5,
     image: "/plats/poulet-yassa.jpg",
   },
   {
     id: 2,
     name: "Mafé",
-    description: "Viande mijotée dans une sauce à l'arachide",
+    description: "Bœuf, sauce arachide, carottes, pommes de terre, tomates, oignons, ail et épices",
     price: 12.5,
     image: "/plats/mafe.jpg",
   },
   {
     id: 3,
     name: "Thiéb",
-    description: "Riz parfumé accompagné de légumes et de viande ou poisson",
+    description: "Riz, poisson ou poulet, légumes et sauce tomate",
     price: 12.5,
     image: "/plats/thieb.jpg",
   },
   {
     id: 4,
     name: "Ndolé",
-    description: "Plat traditionnel camerounais aux feuilles de ndolé",
+    description: "Feuilles de ndolé, arachides, crevettes ou viande, huile de palme, oignons, ail et épices",
     price: 12.5,
     image: "/plats/ndole.jpg",
   },
@@ -37,22 +38,53 @@ export const menu = [
   {
     id: 6,
     name: "Poulet",
-    description: "Poulet savoureux accompagné de sa garniture",
+    description: "Riz basmati, avocat, aiguillettes de poulet grillé, concombre, tomates cerises et edamame",
     price: 12.5,
     image: "/plats/poulet.jpg",
   },
   {
     id: 7,
-    name: "Tartare de saumon",
-    description: "Tartare de saumon frais et savoureux",
+    name: "Tartare au saumon",
+    description: "Saumon frais, avocat, concombre, citron vert et sésame",
     price: 12.5,
     image: "/plats/tartare-saumon.jpg",
   },
   {
     id: 8,
     name: "Salade Wacha",
-    description: "Salade fraîche et gourmande",
+    description: "Brocolis, asperges, courgettes, tomates cerises, mozzarella et edamame",
     price: 12.5,
     image: "/plats/salade-wacha.jpg",
+  },
+
+  // BOISSONS
+  {
+    id: 9,
+    name: "Jus de fruits frais",
+    description: "Orange, carotte, grenade, gingembre,bissap",
+    price: 5,
+    image: "/plats/jus-fruits.jpg",
+  },
+  {
+    id: 10,
+    name: "Softs 50 cl",
+    description: "Eau Volvic, San Pellegrino",
+    price: 2.5,
+    image: "/plats/softs.jpg",
+  },
+  {
+    id: 11,
+    name: "Sodas 33 cl",
+    description:
+      "Coca, Coca Zéro, Sprite, Orangina, Fuze Tea pêche, 7up Mojito, Schweppes agrumes",
+    price: 2,
+    image: "/plats/sodas.jpg",
+  },
+  {
+    id: 12,
+    name: "Bières bouteille 33 cl",
+    description: "Guinness, Cameroun, Peroni, Chimay",
+    price: 5,
+    image: "/plats/bieres.jpg",
   },
 ];
